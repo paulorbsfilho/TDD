f3cd866649ea60a7c86afadb7c756d96a059d76a
paulorbsfilho <p4ulinho@outlook.com>
2018-07-28 12:22:39 -0300

    Mais testes e correção da classe Numero.

--- core/testes.py
+++ core/testes.py
@@ -5,39 +5,31 @@ from .alg_romanos import Numero
 class Test_algromanos(unittest.TestCase):
 
     def test_valores_dos_algarismos_romanos(self):
-        i = Numero.romano_para_int(1)
-        self.assertEqual(i, "I")
-        v = Numero.romano_para_int(5)
-        self.assertEqual(v, "V")
-        x = Numero.romano_para_int(10)
-        self.assertEqual(x, "X")
-        l = Numero.romano_para_int(50)
-        self.assertEqual(l, "L")
-        c = Numero.romano_para_int(100)
-        self.assertEqual(c, "C")
-        d = Numero.romano_para_int(500)
-        self.assertEqual(d, "D")
-        m = Numero.romano_para_int(1000)
-        self.assertEqual(m, "M")
+        n = self.setUp()
+        for i in n:
+            self.assertAlmostEqual()
 
     def test_conversao_valor_algarismos_romanos(self):
-        a = Numero.romano_para_int(123)
+        a = Numero.roman_to_int(123)
         self.assertEqual(a, "CXXIII")
-        b = Numero.romano_para_int(99)
+        b = Numero.roman_to_int(99)
         self.assertEqual(b, "XCIX")
-        c = Numero.romano_para_int(9)
+        c = Numero.roman_to_int(9)
         self.assertEqual(c, "IX")
-        d = Numero.romano_para_int(4)
+        d = Numero.roman_to_int(4)
         self.assertEqual(d, "IV")
 
     def test_repeticao_de_algarismos(self):
-        d = Numero.romano_para_int(4)
-        self.assertEqual(d, "IV")
+        d = Numero.roman_to_int(4)
+        self.assertEqual(d, "VV")
+
         print(d)
 
     def test_tipo_de_input(self):
-        a = Numero.romano_para_int("abc")
+        a = Numero.roman_to_int("abc")
         self.assertRaises(TypeError, a)
+
+
 
 
 if __name__ == '__main__':
